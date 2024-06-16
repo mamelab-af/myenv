80c40435898f678f632efd0e64230435caf289fc
mamelab-af <akira.fukuhara+dev-github@mame-lab.dev>
2024-06-16 16:07:54 +0900

    update

--- run.sh
+++ run.sh
@@ -15,3 +15,9 @@ set -e
   cd homebrew
   ./install-all.sh
 )
+
+##### run knowledge #####
+
+# git clone https://github.com/support-project/docker-knowledge.git
+(cd knowledge; docker-compose up -d)
+# user/password : admin/admin123
