3613484a3eceabbf56a473e8c6ba2d3d4f1a877d
mamelab-af <akira.fukuhara+dev-github@mame-lab.dev>
2025-03-14 16:31:20 +0900

    update

---
+++ windows/github/github-ssh.ps1
@@ -0,0 +1,30 @@
+try {
+  Push-Location $PSScriptRoot
+
+  while ($true) {
+    $email = Read-Host "Enter email address" -AsSecureString
+    if ( $email -and $email -ne "") {
+      break
+    }
+  }
+
+  # Generate key.
+  ssh-keygen -t ed25519 -C $email
+
+  # Copy public key.
+  Get-Content ~/.ssh/id_ed25519.pub | Set-Clipboard
+
+  # Add public key to github(manual).
+  Start-Process "https://github.com/settings/keys"
+
+  # Add to ~/.ssh/config
+  Write-Output "Host github.com
+    IdentityFile ~/.ssh/github
+    User git" > ~/.ssh/config
+
+  # Test
+  ssh -T github.com
+}
+finally {
+  Pop-Location
+}
